f97652ec64586da081a355f42616e0758991ab96
Roy Groot <groot@hotitem.nl>
2021-10-10 16:46:35 +0200

    added rds example

--- rdsExample.py
+++ rdsExample.py
@@ -1,5 +1,18 @@
 import boto3
+import json
 
-client = boto3.client('rds')
+with open('secrets.json') as f:
+    data = json.load(f)
 
-response = 
+rds_client = boto3.client('rds',region_name='eu-west-2', aws_access_key_id=data["access_key_id"],
+                      aws_secret_access_key=data["access_key"])
+response = rds_client.create_db_instance(
+    AllocatedStorage=5,
+    DBInstanceClass='db.t2.micro',
+    DBInstanceIdentifier='roys_database_from_python',
+    Engine='MySQL',
+    MasterUserPassword='testpw0021',
+    MasterUsername='admin01',
+)
+
+print(response)
--- s3_uploading.py
+++ s3_uploading.py
@@ -15,11 +15,13 @@ with open('secrets.json') as f:
 
 file_path = os.path.join(folder, file_name)
 
-s3_resource = boto3.resource('s3', aws_access_key_id=data["access_key_id"],
-                      aws_secret_access_key=data["access_key"])
-
 
 s3_client = boto3.client('s3', aws_access_key_id=data["access_key_id"],
                       aws_secret_access_key=data["access_key"])
+s3_resource = boto3.resource('s3', aws_access_key_id=data["access_key_id"],
+                      aws_secret_access_key=data["access_key"])
+bucket_instance = s3_resource.Bucket(name=bucket)
+object_instance = s3_resource.Object(bucket_name=bucket, key="first_file_name")
 
+object_instance.upload_file("first_file_name")
 s3_resource.Object(bucket, 'latest_uploaded_file.png').upload_file(Filename=file_path)
